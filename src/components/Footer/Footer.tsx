a3c8e1ba8a7c425a5aff4a5f742217c2e0d5b305
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-02-06 02:58:53 +0530

    commit

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <div className="flex flex-col lg:flex-row justify-around px-10 mt-56 gap-20 md:mt-72">
+      <div className="flex flex-col md:flex-row justify-around px-10 mt-56 gap-20 md:mt-72">
         <div className="flex flex-col-reverse items-start md:justify-around md:w-2/3 gap-10 md:gap-36 md:flex-row">
           <div className="flex justify-center gap-10 md:gap-36">
             <div className="">
