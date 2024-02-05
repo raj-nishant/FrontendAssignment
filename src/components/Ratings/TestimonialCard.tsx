070b0e5986329fa2f1dd522691f3f9c3229e8010
Nishant Raj <79581018+raj-nishant@users.noreply.github.com>
2024-02-05 23:54:05 +0530

    commit

--- src/components/Ratings/TestimonialCard.tsx
+++ src/components/Ratings/TestimonialCard.tsx
@@ -7,17 +7,17 @@ const TestimonialCard: React.FC<{
   imageUrl: string;
 }> = ({ rating, quote, description, authorName, authorRole, imageUrl }) => {
   return (
-    <div className="flex flex-col flex-1 p-9 bg-white rounded-3xl border-solid shadow-sm border-[0.722px] border-[color:var(--Neutral-Colors-Color-300,#F0F0F6)] max-md:px-5">
+    <div className="flex flex-col flex-1 p-5 bg-white rounded-3xl border-solid shadow-sm border-[0.722px] border-[color:var(--Neutral-Colors-Color-300,#F0F0F6)] max-md:px-5">
       <div className="text-2xl leading-7 text-slate-800 tracking-[2.89px]">
         {rating}
       </div>
       <div className="mt-9 text-2xl leading-8 text-slate-800">
         &quot;{quote}&quot;
       </div>
-      <div className="mt-2 text-lg leading-8 text-gray-500">
+      <div className="mt-2 text-sm leading-8 text-gray-500">
         &quot;{description}&quot;
       </div>
-      <div className="shrink-0 mt-9 h-px bg-zinc-200" />
+      <div className="shrink-0 mt-2 h-px bg-zinc-200" />
       <div className="flex gap-5 justify-between items-start mt-6">
         <img
           loading="lazy"
